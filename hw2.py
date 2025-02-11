46ac413f1a7d13e72e96c83ad0c0f30fd74cc037
Gul <gul@wifi-10-42-62-229.wifi.berkeley.edu>
2025-02-10 23:18:27 -0800

    Adding homework 2

---
+++ hw2.py
@@ -0,0 +1,21 @@
+# 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+# by entering cd with no other commands can lead you to navigate directly
+# 2) What does ~/ mean?
+# represents the home directory 
+# 3) What's the difference between and abolute and a relative path in your terminal?
+# absolute paths specify the location fromo the root directory while a relative path specifies the location of a file and directory relative current working directory 
+# 4) What command would bring you back to your home directory?
+# running cd without any arguments can bring you back to your home directory
+# 5) If you called rm ./ in your current working directory, what would it do?
+# while the rm command would signify deleting anything in your working directory, in this case it would not do anything as the -r flag would have to be attached in order to delete files  
+# 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+# git add stages the changes and tracks them in any specified files, git commit commits the changes to the local git repository, and git push uploads the commits to a remote repository
+# 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+# it would tell you changes to be commited followed by new file, or modified, or deleted, and if no changes have been made it might say something like nothing to commit 
+# 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+# i think the most frustrating error/bug ive encountered would be getting messages back that a certain file or directory doesnt exist, which i later found that typos were the main source of that to which ive been keen on double checking my commands before entering
+# 9) How does cloning a repository differ from pulling from a repository?
+# cloning a repository makes a copy of a remote repository while pull only copies modified files to the local repository 
+# 10) Tell me a fun fact!
+# penguins propose with pebbles and mate for life!
+# giraffes are more likely to get hit by lightning than humans
